37ba114518316ad6810388a0a6a7750e1d54ce41
candriam <candriam@student.42antananarivo.mg>
2026-08-12 04:58:04 +0300

    Add initialization and exit functions to hello.c

--- task-01-hello-world/hello.c
+++ task-01-hello-world/hello.c
@@ -5,3 +5,17 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("candriam");
 MODULE_DESCRIPTION("Hello World kernel module - Eudyptula Task 01");
+
+static int __init hello_init(void)
+{
+	pr_debug("Hello World!\n");
+	return (0);
+}
+
+static void __exit hello_exit(void)
+{
+	pr_debug("Bye World!\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
