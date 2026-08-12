b11eaa366346abc35376acdbee6c745dc827d4a9
candriam <candriam@student.42antananarivo.mg>
2026-08-12 04:43:17 +0300

    Create task-01-hello-world/hello.c

---
+++ task-01-hello-world/hello.c
@@ -0,0 +1,7 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("candriam");
+MODULE_DESCRIPTION("Hello World kernel module - Eudyptula Task 01");
